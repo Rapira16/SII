41b803a4115baf12a42be44f0e9d53713319534c
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-02-20 19:56:54 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+marks = ['Неуд', 'Удовл', 'Хор', 'Отл']
+
+data = [3, 7, 8, 4]
+fig = plt.figure(figsize=(10, 7))
+plt.pie(data, labels = marks)
+plt.show()
+plt.grid()
+plt.scatter(marks, data)
